63ce01f5bc01274c2f8491881789f453952d7063
Ramaswamy <621955@cognizant.com>
2023-06-21 22:10:58 +0100

    Added building group demo project

---
+++ step-definitions/buildinggroup.step.ts
@@ -0,0 +1,26 @@
+import { Given, When, Then } from '@wdio/cucumber-framework';
+
+
+Given(/^User navigate to application$/, async () => {
+});
+
+When(/^User navigates to building group dialog box$/, async () => {
+});
+
+Then(/^User is in building group dailog box$/, async () => {
+});
+
+Then(/^User selects "([^"]*)" building from list$/, async (name: string) => {
+});
+
+Then(/^User clicks on remove button$/, async () => {
+});
+
+When(/^User clicks on ok in building group dialog box$/, async () => {
+});
+
+When(/^User navigates back to building group dialog box$/, async () => {
+});
+
+When(/^Verify removed building is not displayed in building group list$/, async () => {
+});

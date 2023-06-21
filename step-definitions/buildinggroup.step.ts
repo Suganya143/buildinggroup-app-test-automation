ea05ead0e20c51c3e43f584b96b65046e63a90b3
Suganya143 <121900344+Suganya143@users.noreply.github.com>
2023-06-21 22:24:53 +0100

    Update buildinggroup.step.ts

--- step-definitions/buildinggroup.step.ts
+++ step-definitions/buildinggroup.step.ts
@@ -1,10 +1,17 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
+import { buildingGroupPage } from "../page-objects/buildinggroup.page";
+import { config } from "../wdio.conf";
+import * as commonFunctions from "../commons/common.functions";
 
 
 Given(/^User navigate to application$/, async () => {
+  await commonFunctions.deleteCookies();
+  await commonFunctions.open(config.baseUrl);
 });
 
 When(/^User navigates to building group dialog box$/, async () => {
+  await commonFunctions.waitElementToClick({ element: buildingGroupPage.buildinggroupOption });
+  await commonFunctions.clickElement(buildingGroupPage.buildinggroupOption);
 });
 
 Then(/^User is in building group dailog box$/, async () => {
@@ -14,9 +21,11 @@ Then(/^User selects "([^"]*)" building from list$/, async (name: string) => {
 });
 
 Then(/^User clicks on remove button$/, async () => {
+  await commonFunctions.clickElement(buildingGroupPage.removeButton);
 });
 
 When(/^User clicks on ok in building group dialog box$/, async () => {
+  await commonFunctions.clickElement(buildingGroupPage.okButton);
 });
 
 When(/^User navigates back to building group dialog box$/, async () => {
